a914485a5a584d119c9d64776f98e1c998aa85f8
minoo576 <miny26685@gmail.com>
2025-12-12 00:55:55 -0800

    Delete my-folder directory

--- my-folder/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script compiles and runs the C program.
-
-echo "Compiling vm_fault.c..."
-gcc -O2 -o vm_fault vm_fault.c
-
-echo "Running the program with 4GB..."
-/usr/bin/time -v ./vm_fault 4
